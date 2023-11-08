f4a53cc4f6c73c57bd0e0d7329f4496f583c8aba
Plabon Saha <sahaplabon66@gmail.com>
2023-11-08 13:01:00 +0600

    JWT token secret and set token to http onlu cookie done

--- index.js
+++ index.js
@@ -36,8 +36,14 @@ async function run() {
     app.post('/jwt', async(req,res)=>{
       const user = req.body
       console.log(user);
-      const token = jwt.sign(user,'secret', {expiresIn: '1h'})
-      res.send(token)
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token',token,{
+        httpOnly: true,
+        secure:false,
+        sameSite:'none'
+      })
+      .send({success:true})
     })
     // rooms related API
     app.get('/Rooms',async(req, res)=>{
