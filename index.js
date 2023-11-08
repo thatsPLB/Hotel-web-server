7c3882491ed135a5e53d17a7eedb98b27668a5c7
Plabon Saha <sahaplabon66@gmail.com>
2023-11-08 14:11:01 +0600

    send token server to client

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express =require('express')
 const cors = require('cors');
 const jwt =require('jsonwebtoken')
+const cookieParse= require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 
 console.log(process.env.DB_PASS);
@@ -70,6 +76,7 @@ async function run() {
 
       app.get('/bookings',async(req,res)=>{
         console.log(req.query.email);
+        console.log('tok tok token',req.cookies.token);
         let query ={};
         if(req.query?.email){
           query ={email: req.query.email}
